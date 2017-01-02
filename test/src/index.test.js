b7ef296a418dca9d7076a2c5fab6fc5e38745dd9
Mark Luetke <luetkemj@gmail.com>
2017-01-02 05:08:03 -0600

    partly functional first pass
    works sometimes… there is a bug somewhere at the moment. But even when
    it does work the results are super meh. Should try splitting names into
    syllables or at least letter pairs. Currently this is a typical result
    set:
    'ffoar',
      'squid',
      'ridsq',
      'squsq',
      'jenid',
      'jenid',
      'grids',
      'squsq',
      'manif',
      'fonid'

--- test/src/index.test.js
+++ test/src/index.test.js
@@ -1,8 +1,93 @@
 import should from 'should';
-import proofOfLife from '../../src';
+import * as markov from '../../src';
 
-describe('proofOfLife', () => {
-  it('should exist', () => {
-    should.exist(proofOfLife);
+describe('buildMatrix', () => {
+  it('should work', () => {
+    const expected = {
+      meta: {
+        firstLetters: ['m', 'k'],
+      },
+      chain: {
+        m: { tokens: ['a'], weights: [2] },
+        a: { tokens: ['r', 'm'], weights: [1, 2] },
+        r: { tokens: ['k', 'a'], weights: [1, 2] },
+        k: { tokens: ['r'], weights: [1] },
+      },
+    };
+
+    const actual = markov.buildMatrix(['mark', 'krama']);
+    should(actual).deepEqual(expected);
+  });
+});
+
+describe('generateWeightedMatrix', () => {
+  it('should work', () => {
+    const MATRIX = {
+      m: { a: 2 },
+      a: { r: 1, m: 1 },
+      r: { k: 1, a: 1 },
+      k: { r: 1 },
+    };
+
+    const expected = {
+      m: { tokens: ['a'], weights: [2] },
+      a: { tokens: ['r', 'm'], weights: [1, 2] },
+      r: { tokens: ['k', 'a'], weights: [1, 2] },
+      k: { tokens: ['r'], weights: [1] },
+    };
+
+    const actual = markov.generateWeightedMatrix(MATRIX);
+
+    should(actual).deepEqual(expected);
+  });
+});
+
+
+describe('getToken', () => {
+  it('should work', () => {
+    const ROW = { tokens: ['a', 'b', 'c'], weights: [1, 4, 9] };
+
+    const expected = 'b';
+    const actual = markov.getToken(ROW, 3);
+
+    should(actual).equal(expected);
+  });
+});
+
+describe('walkWeightedMatrix', () => {
+  const MARIX = {
+    meta: {
+      firstLetters: ['m', 'k'],
+    },
+    chain: {
+      m: { tokens: ['a'], weights: [2] },
+      a: { tokens: ['r', 'm'], weights: [1, 2] },
+      r: { tokens: ['k', 'a'], weights: [1, 2] },
+      k: { tokens: ['r'], weights: [1] },
+    },
+  };
+
+  it('should work', () => {
+    const expected = 'krama';
+    const actual = markov.walkWeightedMatrix(MARIX, 5);
+    should(actual.length).equal(expected.length);
+  });
+});
+
+describe('generateNames', () => {
+  const LIST = [
+    'mark',
+    'jeni',
+    'remus',
+    'frances',
+    'squids',
+    'griffon',
+    'joan',
+  ];
+  it('should work', () => {
+    const expected = '';
+    const actual = markov.generateNames(LIST, 10);
+
+    should(actual).equal(expected);
   });
 });
